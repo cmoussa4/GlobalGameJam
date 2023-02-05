7667f2e5b6a8fbf3d1baae60227e7f620d3d85ca
Chris <100540uk@saeinstitute.edu>
2023-02-05 11:05:10 +0000

    Art & Menu

--- Root Defender/Assets/Scripts/Spawner.cs
+++ Root Defender/Assets/Scripts/Spawner.cs
@@ -7,6 +7,7 @@ public class Spawner : MonoBehaviour
     [SerializeField] GameObject Pebbles;
     private float spawnIntervals;
     private float spawnTime = 0f;
+    public float timeTillNextSpawn = 0.1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +17,14 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Random.Range(-11, 11);
+        float x = Random.Range(-8.0f, 8.0f);
         spawnTime += Time.deltaTime;
-        if (spawnTime >= 2.0f)
+        if (spawnTime >= timeTillNextSpawn)
         {
             spawnTime = spawnIntervals;
             Instantiate(Pebbles, new Vector3(x, 5.0f, 0.0f), Quaternion.identity);
+            //timeTillNextSpawn -= 0.1f;
+
         }
     }
 }
